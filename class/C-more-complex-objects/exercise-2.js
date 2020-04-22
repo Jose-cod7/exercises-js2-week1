e72914806b43f446dd2319252be88754714441a0
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-22 20:51:24 +0200

    c-more-complex-objets

--- class/C-more-complex-objects/exercise-2.js
+++ class/C-more-complex-objects/exercise-2.js
@@ -9,14 +9,14 @@ var house = {
     address: "1 Kinning Park",
     previousOwners: ["Claire M.", "John A."],
     currentOwner: {
-      firstName: "Margaret",
-      lastName: "Conway"
-    }
+        firstName: "Margaret",
+        lastName: "Conway",
+    },
 };
 
 var newCurrentOwner = {
     firstName: "Georgina",
-    lastName: "Hernandez"
+    lastName: "Hernandez",
 };
 
 /*
@@ -25,18 +25,24 @@ var newCurrentOwner = {
   WRITE YOUR CODE BELOW
 */
 
+house.currentOwner = newCurrentOwner;
+house.previousOwners[1] = "Stephen B.";
+house.isForSale = false;
+
 // - assign the value of the variable 'newCurrentOwner' as the value to the house's "currentOwner"
 // - from the list of previous owners, replace only "John A." with "Stephen B."
 // - give the house a new property called 'isForSale' with the value 'false'
-
-
-
 
 /*
   DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 console.log(
     "Did you correctly assign the new owner using the given variable?",
-    "Expected result: true. Actual result: " + (house.currentOwner === newCurrentOwner));
-console.log("Expected result: Claire M., Stephen B. Actual result: " + house.previousOwners.toString());
+    "Expected result: true. Actual result: " +
+    (house.currentOwner === newCurrentOwner)
+);
+console.log(
+    "Expected result: Claire M., Stephen B. Actual result: " +
+    house.previousOwners.toString()
+);
 console.log("Expected result: false. Actual result: " + house.isForSale);
